519354c5b2869909b6fbb26072df74ad948d8d51
flyer803 <13521549681@163.com>
2019-05-22 10:40:41 +0800

    接口可以正常访问

---
+++ app/src/main/java/com/cg/requesttest/DialogEvent.java
@@ -0,0 +1,8 @@
+package com.cg.requesttest;
+
+public class DialogEvent {
+    public Boolean needShow;
+    public DialogEvent(boolean needShow) {
+        this.needShow = needShow;
+    }
+}
---
+++ requestapihelper/src/main/java/com/cg/requestapi/request/retrofit/subscriber/DialogEvent.java
@@ -0,0 +1,8 @@
+package com.cg.requestapi.request.retrofit.subscriber;
+
+public class DialogEvent {
+    public Boolean needShow;
+    public DialogEvent(boolean needShow) {
+        this.needShow = needShow;
+    }
+}
